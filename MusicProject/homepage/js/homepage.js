02d9b235d58bb75922e286912fff9188a7564082
Clearlove <1765635281@qq.com>
2018-07-07 08:12:41 +0800

    第二次修改

--- MusicProject/homepage/js/homepage.js
+++ MusicProject/homepage/js/homepage.js
@@ -3,6 +3,13 @@
  */
 $(function () {
 
+    $(".mv_list__title a").hover(function () {
+        $(this).css("color","#31a74a");
+    },function () {
+        $(this).css("color","#5f5f5f");
+    })
+
+
    // 尾部a标签元素悬浮变色
    $(".footer a").hover(function () {
        $(this).css("color","#31a74a");
